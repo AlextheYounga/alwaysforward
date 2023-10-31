23613a1ba95145a551e71171a030a65b5a84a372
Alex Younger <thealexyounger@proton.me>
2023-10-30 22:10:59 -0400

    feat: add navbar

---
+++ resources/js/Components/Nav/NavBar.jsx
@@ -0,0 +1,189 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon, ArrowUpCircleIcon } from '@heroicons/react/24/outline'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar({ route }) {
+    const navigation = [
+        { name: 'Dashboard', href: '/', current: false },
+        { name: 'Life', href: '/life', current: false },
+        { name: 'Week', href: '/week', current: false },
+        { name: 'Goals', href: '/goals', current: false },
+        { name: 'Tasks', href: '/tasks', current: false },
+    ]
+
+    // Set current route to true
+    console.log(route)
+    const currentItem = navigation.find((item) => item.href === route)
+    if (currentItem) currentItem.current = true
+
+
+    const menuItems = [
+        { name: 'Your Profile', href: '#' },
+        { name: 'Settings', href: '#' },
+        { name: 'Sign out', href: '#' },
+    ]
+
+    return (
+        // Desktop Navbar
+        <Disclosure as="nav" className="bg-gray-800">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-4 sm:px-0">
+                        <div className="flex h-16 items-center justify-between">
+                            <div className="flex justify-center items-center">
+                                <div className="text-center text-indigo-500 flex-shrink-0">
+                                    <a href="/">
+                                        <ArrowUpCircleIcon className="h-10 w-10" />
+                                    </a>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {
+                                            navigation.map((item) => (
+                                                <a
+                                                    key={`desktop-${item.name}`}
+                                                    href={item.href} className={classNames(
+                                                        item.current ? 'bg-gray-900 text-white' : '',
+                                                        'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                                                    )}>
+                                                    {item.name}
+                                                </a>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="hidden sm:ml-6 sm:block">
+                                <div className="flex items-center">
+                                    <button
+                                        type="button"
+                                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    >
+                                        <span className="absolute -inset-1.5" />
+                                        <span className="sr-only">View notifications</span>
+                                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+
+                                    {/* Profile dropdown */}
+                                    <Menu as="div" className="relative ml-3">
+                                        <div>
+                                            <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                                <span className="absolute -inset-1.5" />
+                                                <span className="sr-only">Open user menu</span>
+                                                <img
+                                                    className="h-8 w-8 rounded-full"
+                                                    src="https://ui-avatars.com/api/?background=random&name=Alex+Younger"
+                                                    alt=""
+                                                />
+                                            </Menu.Button>
+                                        </div>
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-100"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                {menuItems.map((item) => (
+                                                    <Menu.Item key={`desktop-menu-${item.name}`}>
+                                                        {({ active }) => (
+                                                            <a
+                                                                href={item.href}
+                                                                className={classNames(
+                                                                    active ? 'bg-gray-100' : '',
+                                                                    'block px-4 py-2 text-sm text-gray-700'
+                                                                )}
+                                                            >
+                                                                {item.name}
+                                                            </a>
+                                                        )}
+                                                    </Menu.Item>
+                                                ))
+                                                }
+                                            </Menu.Items>
+                                        </Transition>
+                                    </Menu>
+                                </div>
+                            </div>
+                            <div className="-mr-2 flex sm:hidden">
+                                {/* Mobile menu button */}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Mobile Navbar */}
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    as="a"
+                                    key={`mobile-${item.name}`}
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : '',
+                                        'block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                                    )}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                        <div className="border-t border-gray-700 pb-3 pt-4">
+                            <div className="flex items-center px-5">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="h-10 w-10 rounded-full"
+                                        src="https://ui-avatars.com/api/?background=random&name=Alex+Younger"
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="ml-3">
+                                    <div className="text-base font-medium text-white">Tom Cook</div>
+                                    <div className="text-sm font-medium text-gray-400">tom@example.com</div>
+                                </div>
+                                <button
+                                    type="button"
+                                    className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                >
+                                    <span className="absolute -inset-1.5" />
+                                    <span className="sr-only">View notifications</span>
+                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                </button>
+                            </div>
+                            <div className="mt-3 space-y-1 px-2">
+
+                                {menuItems.map((item) => (
+                                    <Disclosure.Button
+                                        as="a"
+                                        key={`mobile-menu-${item.name}`}
+                                        href={item.href}
+                                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                ))
+                                }
+                            </div>
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
+    )
+}
